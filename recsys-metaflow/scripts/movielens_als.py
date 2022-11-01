abf41c964567b3f3a08d7eea7209f952d860bd9b
Artem Khokhlov <artem.khokhlov@sbermarket.ru>
2022-11-01 23:43:13 +0400

    movielens als

--- recsys-metaflow/scripts/movielens_als.py
+++ recsys-metaflow/scripts/movielens_als.py
@@ -1,30 +1,32 @@
-import logging
 import pandas as pd
+from pandas.api.types import CategoricalDtype
 from scipy import sparse
 import warnings
 
 from recommenders.datasets import movielens
 from implicit.als import AlternatingLeastSquares
-from pandas.api.types import CategoricalDtype
-from metaflow import FlowSpec, step
+from metaflow import FlowSpec, step, project, current
 
 warnings.filterwarnings("ignore")
 
 
+@project(name='recsys')
 class MovielensALSFlow(FlowSpec):
     """
     """
     ratings: pd.DataFrame
-    train_ratings: pd.DataFrame
-    test_ratings: pd.DataFrame
     user_items_train: sparse.csc
 
+    params: dict
     model: AlternatingLeastSquares
 
     @step
     def start(self):
         """
         """
+        print('project name:', current.project_name)
+        print('project branch:', current.branch_name)
+        print('is this a production run?', current.is_production)
         self.next(self.data)
 
     @step
@@ -32,21 +34,10 @@ class MovielensALSFlow(FlowSpec):
         """
         """
         self.ratings = movielens.load_pandas_df(
-            size='1m',
+            size='10m',
             title_col='title',
             genres_col='genre',
-            year_col='year',
         )
-
-        train_ratings, test_ratings = [], []
-        num_test_samples = 10
-
-        for userId, user_data in self.ratings.groupby('userID'):
-            train_ratings += [user_data[:-num_test_samples]]
-            test_ratings += [user_data[-num_test_samples:]]
-
-        self.train_ratings = pd.concat(train_ratings)
-        self.test_ratings = pd.concat(test_ratings)
 
         users = self.ratings["userID"].unique()
         movies = self.ratings["itemID"].unique()
@@ -67,12 +58,16 @@ class MovielensALSFlow(FlowSpec):
     def train(self):
         """
         """
+        self.params = {
+            "iterations": 50,
+            "factors": 50,
+            "regularization": 0.1,
+        }
         self.model = AlternatingLeastSquares(
-            iterations=20,
-            factors=50
+            **self.params
         )
         self.model.fit(
-            self.user_items_train.T
+            self.user_items_train
         )
 
         self.next(self.end)
@@ -81,7 +76,6 @@ class MovielensALSFlow(FlowSpec):
     def end(self):
         """
         """
-        logging.info(self.model.similar_items(1))
 
 
 if __name__ == "__main__":
